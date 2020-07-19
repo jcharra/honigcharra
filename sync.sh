19897f00bdab322b99391f63778e12433ca32e05
Johannes Charra <johannes.charra@haufe-lexware.net>
2020-07-19 20:30:19 +0200

    link larger image

--- sync.sh
+++ sync.sh
@@ -3,5 +3,6 @@ scp *.html jcharra@207.38.94.46:webapps/honigcharra/
 scp *.svg jcharra@207.38.94.46:webapps/honigcharra/
 scp *.css jcharra@207.38.94.46:webapps/honigcharra/
 scp *.js jcharra@207.38.94.46:webapps/honigcharra/
+scp *.jpg jcharra@207.38.94.46:webapps/honigcharra/
 
 
