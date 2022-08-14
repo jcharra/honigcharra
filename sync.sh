0f5bc69e5c04c1037b0e65283763c0a3653c7b7f
Johannes Charra <johannes.charra@gmail.com>
2022-08-14 15:20:03 +0200

    prepare github pages

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-scp *.html strato:/var/www/honig-charra.de/public_html/
-scp *.svg strato:/var/www/honig-charra.de/public_html/
-scp *.css strato:/var/www/honig-charra.de/public_html/
-scp *.js strato:/var/www/honig-charra.de/public_html/
-scp *.jpg strato:/var/www/honig-charra.de/public_html/
-
-
